89880c5e993a8b4d21a7d7678cff3a07b39a98e9
Aquila0911 <siddharth.ghosh9@gmail.com>
2024-07-02 11:01:24 +0530

    some console log change

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   mongoose
     .connect(process.env.MONGO_URI)
-    .then(() => console.log("Connected to database THETAONE-DB..."))
+    .then(() => console.log("Connected to database..."))
     .catch((err) => console.error("Error connecting to database: ", err));
 };
 
